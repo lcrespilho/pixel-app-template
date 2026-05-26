24dedd0a4728dc2a6eb61f4982bd855efcad7be0
Leonardo Lourenço Crespilho <leonardo.lourenco@monks.com>
2026-05-26 00:34:16 -0300

    test1

--- react/index.tsx
+++ react/index.tsx
@@ -3,6 +3,8 @@ import { canUseDOM } from 'vtex.render-runtime'
 import type { PixelMessage } from './typings/events'
 
 export function handleEvents(e: PixelMessage) {
+  // eslint-disable-next-line no-console
+  console.log('>> handleEvents:', e)
   switch (e.data.eventName) {
     case 'vtex:pageView': {
       break
